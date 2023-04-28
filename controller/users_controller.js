a1ef9140987cbe076ae9c29bc25c305061133b9f
Kritie Agarwal <kritieagarwal@gmail.com>
2023-04-29 00:54:51 +0530

    added user controller

---
+++ controller/users_controller.js
@@ -0,0 +1,8 @@
+module.exports.profile = function (req, res) {
+    res.end('<h1>User Profile</h1>');
+    console.log('profile');
+}
+
+// module.exports.posts = function (req, res) {
+//     res.end('<h1>Users Posts</h1>');
+// }
