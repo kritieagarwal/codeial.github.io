008e54ebfe61665e1668baf0f900a4df657632a2
Kritie Agarwal <kritieagarwal@gmail.com>
2023-04-29 02:19:07 +0530

    added view controller for user

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,7 +1,13 @@
 module.exports.profile = function (req, res) {
-    res.end('<h1>User Profile</h1>');
+    // res.end('<h1>User Profile</h1>');
+    return res.render('users', {
+        title: "profile"
+    });
 }
 
 module.exports.posts = function (req, res) {
-    res.end('<h1>Posts</h1>');
+    // res.end('<h1>Posts</h1>');
+    return res.render('users', {
+        title: "posts"
+    });
 }
