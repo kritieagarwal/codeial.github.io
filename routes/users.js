0ad62a10bb952596e8fd4287ab15e0a8a5d9f427
Kritie Agarwal <kritieagarwal@gmail.com>
2023-04-30 21:01:29 +0530

    added signup functionality

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,9 @@ router.get('/profile', usersController.profile);
 
 router.get('/posts', usersController.posts);
 
+router.get('/sign-up', usersController.signup);
+router.get('/sign-in', usersController.signin);
+
+router.post('/create', usersController.create);
+
 module.exports = router;
