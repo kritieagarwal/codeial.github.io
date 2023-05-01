8aac6c0020fd089e9be4786e8986ec0e23db102b
Kritie Agarwal <kritieagarwal@gmail.com>
2023-05-01 16:22:16 +0530

    manual authentication for sign in

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,12 +1,51 @@
 const User = require('../models/users');
+const { use } = require('../routes/users');
 
+
+// module.exports.profile = function (req, res) {
+//     if (req.cookies.user_id) {
+//         User.findById(req.cookies.user_id)
+//             .then(user => {
+//                 if (user) {
+//                     return res.render('users', {
+//                         title: 'User Profile',
+//                         user: user,
+//                     })
+//                 }
+//                 else {
+//                     return res.redirect('/users/sign-in');
+//                 }
+//             });
+//     }
+//     else {
+//         return res.redirect('/users/sign-in');
+//     }
+// }
 
 module.exports.profile = function (req, res) {
-    // res.end('<h1>User Profile</h1>');
-    return res.render('users', {
-        title: "profile"
-    });
+    if (req.cookies.user_id) {
+        User.findById(req.cookies.user_id)
+            .then(user => {
+                if (user) {
+                    return res.render('users', {
+                        title: 'User Profile',
+                        user: user,
+                    })
+                }
+                else {
+                    return res.redirect('/users/sign-in');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                return res.redirect('/users/sign-in');
+            });
+    }
+    else {
+        return res.redirect('/users/sign-in');
+    }
 }
+
 
 module.exports.posts = function (req, res) {
     // res.end('<h1>Posts</h1>');
@@ -35,22 +74,6 @@ module.exports.create = function (req, res) {
         return res.redirect('back');
     }
 
-    // User.findOne({ email: req.body.email }, function (err, user) {
-    //     if (err) { console.log('error in finding user in signing up'); return }
-
-    //     if (!user) {
-    //         User.create(req.body, function (err, user) {
-    //             if (err) { console.log('error in creating user while signing up'); return }
-
-    //             return res.redirect('/users/sign-in');
-    //         })
-    //     }
-    //     else {
-    //         return res.redirect('back');
-    //     }
-
-    // })
-
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
@@ -72,5 +95,42 @@ module.exports.create = function (req, res) {
 
 // sign-in and create a session for the user
 module.exports.createSession = function (req, res) {
-    // todo later
+
+    // steps to authenticate
+    // find the user
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+
+            if (user) {
+                // handle password that doesn't match
+                if (user.password != req.body.password) {
+                    return res.redirect('back');
+                }
+
+                // handle session creation
+                res.cookie('user_id', user.id);
+                return res.redirect('/users/profile');
+
+                // handle user not found
+
+            }
+            else {
+                return res.redirect('back');
+            }
+
+        })
+        .catch(err => {
+            console.log('error in finding user in signing in', err);
+        });
 }
+
+// module.exports.delete = function (req, res) {
+//     if (req.cookies.user_id) {
+//         res.clearCookie('user_id');
+//         res.redirect('/users/sign-in');
+//     }
+//     else {
+//         res.redirect('back');
+//     }
+// }
--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,8 @@ router.get('/posts', usersController.posts);
 router.get('/sign-up', usersController.signup);
 router.get('/sign-in', usersController.signin);
 
+// router.post('/signout', usersController.delete);
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
 
 module.exports = router;
