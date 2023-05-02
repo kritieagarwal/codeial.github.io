025975a7061d5026385e4fd1d353591b962a0272
Kritie Agarwal <kritieagarwal@gmail.com>
2023-05-02 17:24:41 +0530

    added post functionality

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -44,22 +44,6 @@ module.exports.create = function (req, res) {
         return res.redirect('back');
     }
 
-    // User.findOne({ email: req.body.email }, function (err, user) {
-    //     if (err) { console.log('error in finding user in signing up'); return }
-
-    //     if (!user) {
-    //         User.create(req.body, function (err, user) {
-    //             if (err) { console.log('error in creating user while signing up'); return }
-
-    //             return res.redirect('/users/sign-in');
-    //         })
-    //     }
-    //     else {
-    //         return res.redirect('back');
-    //     }
-
-    // })
-
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
@@ -85,6 +69,11 @@ module.exports.createSession = function (req, res) {
 }
 
 module.exports.destroySession = function (req, res) {
-    req.logout();
+    req.logout(function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
     return res.redirect('/');
 }
