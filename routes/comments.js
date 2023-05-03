6646c7f3642359450f02cc28f0aaf504e14f7293
Kritie Agarwal <kritieagarwal@gmail.com>
2023-05-03 13:29:13 +0530

    added comment delete functionality

--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,6 @@ const commentsController = require('../controller/comments_controller');
 
 // checking authentication so that only signed in user can only post
 router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;
