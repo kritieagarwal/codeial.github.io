43a6b96e7ecf78297a755ab29149ff771d419fd2
Kritie Agarwal <kritieagarwal@gmail.com>
2023-05-05 02:23:27 +0530

    added ajax to comments and file upload functionlity using multer

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/users');
-
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function (req, res) {
     User.findById(req.params.id)
@@ -12,18 +13,41 @@ module.exports.profile = function (req, res) {
 
 }
 
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
     if (req.user.id == req.params.id) {
-        User.findByIdAndUpdate(req.params.id, req.body)
-            .then(user => {
-                req.flash('success', 'Profile updated!');
+
+        try {
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function (err) {
+                if (err) { console.log('****Multer Error: ', err) }
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file) {
+
+                    if (user.avatar) {
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+
+
+                    // this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
                 return res.redirect('back');
             });
-    }
-    else {
+        } catch (err) {
+            req.flash('error', err);
+            return res.redirect('back');
+        }
+
+    } else {
+        req.flash('error', 'Unautorized!');
         return res.status(401).send('Unauthorized');
     }
 }
+
 
 module.exports.posts = function (req, res) {
     // res.end('<h1>Posts</h1>');
