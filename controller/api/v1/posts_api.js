0ecd7f48227df06fdb40445bfb961b44203684ec
Kritie Agarwal <kritieagarwal@gmail.com>
2023-05-05 12:54:23 +0530

    accessed api

---
+++ controller/api/v1/posts_api.js
@@ -0,0 +1,57 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+
+    return res.json(200, {
+        message: "List of Posts",
+        posts: posts
+    })
+}
+
+
+module.exports.destroy = async function (req, res) {
+
+    try {
+        let post = await Post.findById(req.params.id);
+
+        // .id means converting the object id into string
+        // if (post.user == req.user.id) {
+        post.deleteOne();
+
+        await Comment.deleteMany({ post: req.params.id });
+
+
+
+        // req.flash('success', 'Post and associated comments deleted!');
+
+        return res.json(200, {
+            message: "Posts and associated commets deleted"
+        });
+
+        // }
+        // else {
+        //     req.flash('error', 'You cannot delete this post!');
+        //     return res.redirect('back');
+        // }
+    }
+    catch (err) {
+        // req.flash('error', err);
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
+
+}
